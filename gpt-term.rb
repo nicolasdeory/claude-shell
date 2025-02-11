284a8839f0162d72fcef06d78526a3b25c37a18a
Nico de Ory <nicolas.deory@homagames.com>
2025-02-11 15:21:44 +0100

    formula

--- gpt-term.rb
+++ gpt-term.rb
@@ -5,10 +5,10 @@ class GptTerm < Formula
     
     if OS.mac?
       url "https://github.com/nicolasdeory/claude-shell/releases/download/v1.0.0/gpt-term-darwin-amd64"
-      sha256 "YOUR_DARWIN_BINARY_SHA256"
+      sha256 "143144be990e8017721a1030aadb1915bb732aaf8510062c3978fc09a0dc578b"
     elsif OS.linux?
       url "https://github.com/nicolasdeory/claude-shell/releases/download/v1.0.0/gpt-term-linux-amd64"
-      sha256 "YOUR_LINUX_BINARY_SHA256"
+      sha256 "bc8df7de075e87a193c6e8c629d1b063932ba4770ebe8b766f366ea3023399fa"
     end
     
     def install
